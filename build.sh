bbbb5048edee8d906b37752926d99d51edfe3f19
Aleksander Kozminski <alek.k@berkeley.edu>
2025-12-19 02:23:17 -0700

    feat: source maps

--- build.sh
+++ build.sh
@@ -37,14 +37,15 @@ compile_all_ts() {
     num_solver_files=${#solver_files[@]}
 
     google-closure-compiler -O ADVANCED --module_resolution=NODE --chunk_output_type=ES_MODULES --externs ts/ui/externs.js \
+    --create_source_map "%outname%.map" --source_map_location_mapping "ts/|/ts/" \
     --chunk_output_path_prefix intermediate/ \
     --chunk="common:${num_common_files}::${repo_root}/js/closure/common/common.js" ${common_files[@]} \
     --chunk="ui:${num_ui_files}:common" ${ui_files[@]} \
     --chunk="solver:${num_solver_files}:common:${repo_root}/dist/bolver/dolver.min.js" ${solver_files[@]} $debug
 
-    google-closure-compiler -O SIMPLE --module_resolution=NODE --externs ts/ui/externs.js --chunk_output_type=ES_MODULES intermediate/common.js intermediate/ui.js --js_output_file dist/ui/ui.min.mjs $debug
+    google-closure-compiler -O SIMPLE --module_resolution=NODE --externs ts/ui/externs.js --create_source_map "%outname%.map" --source_map_input "intermediate/common.js|intermediate/common.js.map" --source_map_input "intermediate/ui.js|intermediate/ui.js.map" --output_wrapper "%output%//# sourceMappingURL=ui.min.mjs.map" --chunk_output_type=ES_MODULES intermediate/common.js intermediate/ui.js --js_output_file dist/ui/ui.min.mjs $debug
 
-    google-closure-compiler -O SIMPLE --module_resolution=NODE --chunk_output_type=ES_MODULES intermediate/common.js intermediate/solver.js --js_output_file dist/solver/solver.min.mjs $debug
+    google-closure-compiler -O SIMPLE --module_resolution=NODE --create_source_map "%outname%.map" --source_map_input "intermediate/common.js|intermediate/common.js.map" --source_map_input "intermediate/solver.js|intermediate/solver.js.map" --output_wrapper "%output%//# sourceMappingURL=solver.min.mjs.map" --chunk_output_type=ES_MODULES intermediate/common.js intermediate/solver.js --js_output_file dist/solver/solver.min.mjs $debug
 
     echo -e "\033[34mClosure compilation done\033[0m"
 }
